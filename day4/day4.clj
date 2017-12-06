215b6b8586544a2ce3b0f505c5ef3516a15a3874
Nabil Benketaf <nbenketaf@gmail.com>
2017-12-07 00:09:02 +0100

    Update day 4

--- day4/day4.clj
+++ day4/day4.clj
@@ -4,20 +4,15 @@
        str/split-lines
        (map #(str/split % #" "))))
 
-(defn solve [valid? colls]
-  (->> (filter valid? colls)
+(defn solve [f colls]
+  (->> (filter #(->> (map f %)
+                     (apply distinct?)) colls)
        count))
-
-(defn anagram [coll]
-  (->> coll
-       (group-by sort)
-       vals
-       (filter #(> (count %) 1))))
 
 (defn day4-1 [filename]
   (let [colls (parse filename)
-        res1 (solve #(apply distinct? %) colls)
-        res2 (solve #(empty? (anagram %)) colls)]
+        res1 (solve identity colls)
+        res2 (solve sort colls)]
     (println res1)
     (println res2)))
 
