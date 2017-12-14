bd1ba6557a904bdf6c25eaab9023c582cd001429
Nabil Benketaf <nbenketaf@gmail.com>
2017-12-14 02:23:21 +0100

    Add day7 part 1

---
+++ day7/day7.clj
@@ -0,0 +1,20 @@
+(def line-regex #"(\w+)\s\((\d+)\)\s?-?>?\s?(.*)")
+
+(defn parse [filename]
+  (->> filename
+       slurp
+       clojure.string/split-lines
+       (map #(re-matches line-regex %))))
+
+(defn solve [input]
+  (let [[holding holded] (->> input
+                              (filter (comp not-empty last))
+                              (map (juxt second last))
+                              (reduce (fn [[holding holded] line]
+                                        [(conj holding (first line)) (->> (clojure.string/split (last line) #",")
+                                                                          (map clojure.string/trim)
+                                                                          (apply conj holded))])
+                                      [#{} #{}]))]
+    (->> (clojure.set/intersection holding holded)
+         (clojure.set/difference holding)
+         first)))
